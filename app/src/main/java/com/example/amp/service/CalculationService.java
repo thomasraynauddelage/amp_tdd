caf11b1d485d1e1525c2cb03fd94051c717bbc1b
Thomas Raynaud de Lage <thomas.raynauddelage@mail.mcgill.ca>
2016-02-18 16:33:42 -0500

    Fix boundaries

--- app/src/main/java/com/example/amp/service/CalculationService.java
+++ app/src/main/java/com/example/amp/service/CalculationService.java
@@ -8,14 +8,15 @@ public class CalculationService {
     public static double getPrice(double width, double length, double weight, String destination, String type) {
         double price = 0.00;
 
-        if((width <= 90||width >= 270)){
+        if(width < 90||width > 270){
             throw new IllegalArgumentException("Width must be between 90mm and 270mm ");
         }
-        if(length <= 140 || length >= 380 ){
-            throw new IllegalArgumentException("Length must be between 140mm ");
+
+        if(length < 140 || length > 380 ){
+            throw new IllegalArgumentException("Length must be between 140mm and 380mm ");
         }
-        if(weight<=3 || weight >= 500){
-            throw new IllegalArgumentException("Weight must be between 3.0g ");
+        if(weight<3 || weight > 500){
+            throw new IllegalArgumentException("Weight must be between 3.0g and 500.0g ");
         }
         if(!destination.equals("Canada") && !destination.equals("United States") && !destination.equals("International")){
             throw new IllegalArgumentException("Not a valid destination");
@@ -168,36 +169,6 @@ public class CalculationService {
             }
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         return price;
 
